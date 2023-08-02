120d53cae0723a3c2b3edee6e39a58cbe5643f9a
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-08-02 16:28:27 -0400

    finished

--- app.js
+++ app.js
@@ -3,34 +3,29 @@ const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slideLength = slideRight.querySelectorAll('div').length
+const slidesLength = slideRight.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-slideLeft.computedStyleMap.top = `-${(slideLength-1)*100}vh`
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
-
-downButton.addEventListener('click', ()=> changeSlide('down'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
-
     if(direction === 'up') {
         activeSlideIndex++
-
-        if(activeSlideIndex > slideLength - 1) {
+        if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
         }
     } else if(direction === 'down') {
         activeSlideIndex--
-
         if(activeSlideIndex < 0) {
-            activeSlideIndex = slideLength -1
+            activeSlideIndex = slidesLength - 1
         }
     }
 
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
-
     slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
